9ad9d6483b62b394e4a7e234288d819e37e2c112
ls <shuai.lu@goodark.com>
2023-03-29 09:08:26 +0800

    ls

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,114 +2,118 @@
  * @Author: ls shuai.lu@goodark.com
  * @Date: 2023-03-22 14:17:17
  * @LastEditors: ls shuai.lu@goodark.com
- * @LastEditTime: 2023-03-28 15:50:31
+ * @LastEditTime: 2023-03-29 09:07:35
  * @FilePath: \vueproject2\src\view\login.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-    <template>
-        <div class="login-container">
-          <form class="login-form" @submit.prevent="submitForm">
-            <h2 class="login-title">asd</h2>
-            <div class="form-group">
-              <label for="username">asd</label>
-              <input id="username" type="text" v-model="username" required>
-            </div>
-            <div class="form-group">
-              <label for="password">asd</label>
-              <input id="password" type="password" v-model="password" required autocomplete="current-password">
-            </div>
-            <button class="login-btn" type="submit" id="asd" >登录</button>
-            <button class="login-btn" type="primary" >注册</button>
-          </form>
-        </div>
-      </template>
-<script>
 
+ <template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section class="hero">
+        <h1>Welcome to my website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin, ante eu gravida imperdiet, dolor quam ornare nisi, nec sollicitudin lorem est vel dui.</p>
+        <button>Learn More</button>
+      </section>
+      <section class="features">
+        <div class="feature">
+          <img src="image1.jpg" alt="Feature 1">
+          <h2>Feature 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin, ante eu gravida imperdiet, dolor quam ornare nisi, nec sollicitudin lorem est vel dui.</p>
+        </div>
+        <div class="feature">
+          <img src="image2.jpg" alt="Feature 2">
+          <h2>Feature 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin, ante eu gravida imperdiet, dolor quam ornare nisi, nec sollicitudin lorem est vel dui.</p>
+        </div>
+        <div class="feature">
+          <img src="image3.jpg" alt="Feature 3">
+          <h2>Feature 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin, ante eu gravida imperdiet, dolor quam ornare nisi, nec sollicitudin lorem est vel dui.</p>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <p>&copy; 2023 My Website</p>
+    </footer>
+  </div>
+</template>
+
+<script>
 export default {
-  data () {
-    return {
-      username: '',
-      password: ''
-    }
-  },
-  methods: {
-    // submitForm () {
-    //   // 提交表单的逻辑，例如发起登录请求
-    //   this.$router.push('/home')
-    // }
-    submitForm () {
-      // 模拟验证用户名和密码
-      if (this.username === 'admin' && this.password === 'admin') {
-      // 验证通过，跳转到主页面
-        this.$router.push('/home')
-      } else {
-      // 验证失败，提示用户重新输入
-        alert('请输入正确的用户名或密码')
-      }
-    }
-  }
+  name: 'App'
 }
 </script>
 
-      <style scoped>
-      .login-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background-color: #f5f5f5;
-      }
-      .login-form {
-        width: 360px;
-        padding: 40px;
-        background-color: #fff;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-      .login-title {
-        margin-top: 0;
-        margin-bottom: 20px;
-        font-size: 24px;
-        font-weight: bold;
-        text-align: center;
-      }
-      .form-group {
-        margin-bottom: 20px;
-      }
-      label {
-        display: block;
-        margin-bottom: 5px;
-        font-size: 14px;
-        font-weight: bold;
-      }
-      input {
-        display: block;
-        width: 100%;
-        height: 40px;
-        padding: 0 10px;
-        font-size: 16px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-        box-sizing: border-box;
-        outline: none;
-        transition: border-color 0.3s ease;
-      }
-      input:focus {
-        border-color: #007aff;
-      }
-      .login-btn {
-        width: 30%;
-        height: 40px;
-        line-height: 40px;
-        margin-top: 20px;
-        font-size: 16px;
-        color: #fff;
-        background-color: #007aff;
-        border: none;
-        border-radius: 10px;
-        cursor: pointer;
-      }
-      .login-btn:hover {
-        background-color: #006be0;
-      }
-      </style>
+<style>
+/* Reset default browser styles */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+/* Set up basic page styles */
+body {
+  font-family: "Arial", sans-serif;
+}
+
+header {
+  background-color: #222;
+  color: #fff;
+  padding: 1rem;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+}
+
+nav ul li {
+  margin-right: 1rem;
+}
+
+nav ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+
+main {
+  padding: 2rem;
+}
+
+.hero {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #ccc;
+  height: 60vh;
+}
+
+.hero h1 {
+  font-size: 4rem;
+  margin-bottom: 1rem;
+}
+
+.hero p {
+  margin-bottom: 1.5rem;
+}
+ </style>
--- src/view/login.vue
+++ src/view/login.vue
@@ -1,12 +1,10 @@
-<template>
+<!-- <template>
     <div>
-      <!-- 顶部搜索框 -->
       <div class="search-bar">
         <input type="text" placeholder="输入关键词搜索">
         <button>搜索</button>
       </div>
 
-      <!-- 中间热门推荐滚动图 -->
       <div class="hot-recommendations">
         <h2>热门推荐</h2>
         <div class="slider">
@@ -14,7 +12,6 @@
         </div>
       </div>
 
-      <!-- 预算、地点、天数表单 -->
       <div class="form">
         <label>预算：</label>
         <input type="number" v-model="budget">
@@ -109,4 +106,111 @@ export default {
     justify-content: center;
     align-items: center;
     margin: 50px 0;
-  }</style>
+  }</style> -->
+  <template>
+    <div class="login-container">
+      <form class="login-form" @submit.prevent="submitForm">
+        <h2 class="login-title">登录</h2>
+        <div class="form-group">
+          <label for="username">用户名</label>
+          <input id="username" type="text" v-model="username" required>
+        </div>
+        <div class="form-group">
+          <label for="password">密码</label>
+          <input id="password" type="password" v-model="password" required autocomplete="current-password">
+        </div>
+        <button class="login-btn" type="submit" id="asd" >登录</button>
+        <button class="login-btn" type="primary" >注册</button>
+      </form>
+    </div>
+  </template>
+<script>
+
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    // submitForm () {
+    //   // 提交表单的逻辑，例如发起登录请求
+    //   this.$router.push('/home')
+    // }
+    submitForm () {
+      // 模拟验证用户名和密码
+      if (this.username === 'admin' && this.password === 'admin') {
+        // 验证通过，跳转到主页面
+        this.$router.push('/home')
+      } else {
+        // 验证失败，提示用户重新输入
+        alert('请输入正确的用户名或密码')
+      }
+    }
+  }
+}
+</script>
+
+  <style scoped>
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f5f5f5;
+  }
+  .login-form {
+    width: 360px;
+    padding: 40px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  .login-title {
+    margin-top: 0;
+    margin-bottom: 20px;
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .form-group {
+    margin-bottom: 20px;
+  }
+  label {
+    display: block;
+    margin-bottom: 5px;
+    font-size: 14px;
+    font-weight: bold;
+  }
+  input {
+    display: block;
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    font-size: 16px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-sizing: border-box;
+    outline: none;
+    transition: border-color 0.3s ease;
+  }
+  input:focus {
+    border-color: #007aff;
+  }
+  .login-btn {
+    width: 30%;
+    height: 40px;
+    line-height: 40px;
+    margin-top: 20px;
+    font-size: 16px;
+    color: #fff;
+    background-color: #007aff;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .login-btn:hover {
+    background-color: #006be0;
+  }
+  </style>
